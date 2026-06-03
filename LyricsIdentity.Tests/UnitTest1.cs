1c6d5e308f54b465fe497022eafd380163e275c8
Nethrazim <vlad.branzei@gmail.com>
2026-06-03 04:45:58 +0300

    + init project structure

---
+++ LyricsIdentity.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace LyricsIdentity.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var forecast = new LyricsIdentity.WeatherForecast
+        {
+            TemperatureC = 0
+        };
+
+        Assert.Equal(32, forecast.TemperatureF);
+    }
+}
