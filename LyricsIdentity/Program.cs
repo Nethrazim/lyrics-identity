ccc100bc08d61aa97241b80e59c252137319ce9d
Nethrazim <vlad.branzei@gmail.com>
2026-06-03 05:43:43 +0300

    + Add Logging

--- LyricsIdentity/Program.cs
+++ LyricsIdentity/Program.cs
@@ -20,6 +20,21 @@ public class Program
         app.UseSwagger();
         app.UseSwaggerUI();
 
+        app.Use(async (context, next) =>
+        {
+            var startTime = DateTimeOffset.UtcNow;
+
+            await next();
+
+            var elapsedMilliseconds = (DateTimeOffset.UtcNow - startTime).TotalMilliseconds;
+            app.Logger.LogInformation(
+                "{Method} {Path} responded {StatusCode} in {ElapsedMilliseconds:F0} ms",
+                context.Request.Method,
+                context.Request.Path,
+                context.Response.StatusCode,
+                elapsedMilliseconds);
+        });
+
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
